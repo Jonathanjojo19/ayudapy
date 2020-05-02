fa468b438ae46ede6f6f8cee35237c72ae9312fe
Jonathanjojo <jonathanchristopher1199@gmail.com>
2020-05-02 16:03:21 +0700

    Add loadenv

--- conf/settings.py
+++ conf/settings.py
@@ -7,6 +7,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import environ
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
